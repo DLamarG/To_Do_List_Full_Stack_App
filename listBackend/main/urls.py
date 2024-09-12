feecf67a35d8b7d32d9df8a1baceef6bdbf1f939
DLamarG <demondg8@gmail.com>
2024-09-12 10:07:20 -0500

    removed app

--- listBackend/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserTaskListView, UserTaskByCategoryView, TaskCreateView
-
-urlpatterns = [
-    path('tasks/', UserTaskListView.as_view(), name='user-task-list'),
-    path('tasks/category/<int:category_id>/', UserTaskByCategoryView.as_view(), name='user-task-by-category'),
-    path('tasks/create/', TaskCreateView.as_view(), name='task-create'),
-]
