52c8201283db3b0fa7c4c6f0c9ae46311f700327
DLamarG <demondg8@gmail.com>
2024-09-14 21:21:38 -0500

    updated the model

--- listBackend/main/models.py
+++ listBackend/main/models.py
@@ -2,7 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+TITLE_CHOICES = {
+    "Applications",
+    "Networking",
+    "Interviews",
+    "Coding",
+    "Projects",
+    "Certifications",
+}
 
-# Create your models here.
+#Author model.
 class Author(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField(null=True)
+
+
+
+# Category
+class Category(models.Model):
+    title = models.CharField(max_length=15, choices=TITLE_CHOICES)
+    details=models.TextField(null=True, max_length=500)
