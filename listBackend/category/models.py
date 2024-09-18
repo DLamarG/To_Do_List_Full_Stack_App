38f446b9a8e59e8d55f65a57fc31f256fc8523a8
DLamarG <demondg8@gmail.com>
2024-09-18 16:12:05 -0500

    added model to task_category app

--- listBackend/category/models.py
+++ listBackend/category/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+TITLE_CHOICES = {
+    "Applications",
+    "Networking",
+    "Interviews",
+    "Coding",
+    "Projects",
+    "Certifications",
+}
+
+
+
+
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=15, choices=TITLE_CHOICES)
+    details=models.TextField(null=True, max_length=500)
+    category_id=models.CharField(primary_key=True)
