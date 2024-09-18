ffa973c5f9c699ae21d5f60d9d7c716cf0380a8e
DLamarG <demondg8@gmail.com>
2024-09-18 16:34:06 -0500

    added more code to the main application, added a new model in this application

--- listBackend/main/models.py
+++ listBackend/main/models.py
@@ -8,16 +8,16 @@ from task.models import Task
 #Author model.
 class Author(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
-    my_recipes = models.ManyToManyField(Task, related_name='my_recipes', blank=True)
+    my_task = models.ManyToManyField(Task, related_name='my_task', blank=True)
 
 
     def add_to_recipes(self, task_title, task_details, task_id):
-        if not self.my_recipes.filter(task_title=task_title).exists():
+        if not self.my_task.filter(task_title=task_title).exists():
             recipe=Task.objects.create(task_id=task_id, task_title=task_title, task_details=task_details)
-            self.my_recipes.add(recipe)
-            return {'message': 'recipe added to your recipes'}
+            self.my_task.add(recipe)
+            return {'message': 'task added to your list of tasks'}
         else:
-            return {'message': 'recipe already exist'}
+            return {'message': 'task already exist'}
         
 
     def __str__(self):
