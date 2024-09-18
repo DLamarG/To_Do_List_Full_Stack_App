88ef3b5cf4e47274e83b5ec9fdfb5b8fc957e0b5
DLamarG <demondg8@gmail.com>
2024-09-18 16:25:46 -0500

    added author model to main application

--- listBackend/main/models.py
+++ listBackend/main/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+from task.models import Task
 
 
 
 
 #Author model.
 class Author(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    message = models.TextField(null=True)
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    my_recipes = models.ManyToManyField(Task, related_name='my_recipes', blank=True)
+
+
+    def add_to_recipes(self, task_title, task_details, task_id):
+        if not self.my_recipes.filter(task_title=task_title).exists():
+            recipe=Task.objects.create(task_id=task_id, task_title=task_title, task_details=task_details)
+            self.my_recipes.add(recipe)
+            return {'message': 'recipe added to your recipes'}
+        else:
+            return {'message': 'recipe already exist'}
+        
+
+    def __str__(self):
+        return self.user.username
 
 
 
