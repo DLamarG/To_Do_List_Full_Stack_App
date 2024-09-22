61d346aed315c2ee01485dc69ea8db1c3fd8a4bf
DLamarG <demondg8@gmail.com>
2024-09-22 17:41:07 -0500

    serializer complete

--- listBackend/task/serializers.py
+++ listBackend/task/serializers.py
@@ -4,4 +4,5 @@ from . import models
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
-        
+        model=models.Task
+        fields=['task_id'], ['task_title'], ['task_details']
