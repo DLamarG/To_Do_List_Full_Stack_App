f3b893c5f991f2e08a61dd41d132ebc0a5caaa15
DLamarG <demondg8@gmail.com>
2024-09-23 16:59:03 -0500

    updated task model to include the category

--- listBackend/task/models.py
+++ listBackend/task/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from ..category.models import Category
 
 # Create your models here.
 class Task(models.Model):
     task_id = models.AutoField(primary_key=True)
     task_title = models.CharField(max_length=50, blank=False)
     task_details = models.CharField(max_length=1000, blank=False)
-    # picture = models.FileField(upload_to="foodapp_pics", blank=True, null=True)
+    task_details = models.CharField(max_length=1000, blank=False)
+    category = models.ForeignKey(Category, related_name="tasks", on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.task_id
+        return self.task_title
