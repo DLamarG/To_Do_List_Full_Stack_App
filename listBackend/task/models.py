addc41e073958827b26325275f6a6bfb7ac3eccf
DLamarG <demondg8@gmail.com>
2024-09-23 17:02:40 -0500

    added new field to task serializer

--- listBackend/task/models.py
+++ listBackend/task/models.py
@@ -6,7 +6,7 @@ class Task(models.Model):
     task_id = models.AutoField(primary_key=True)
     task_title = models.CharField(max_length=50, blank=False)
     task_details = models.CharField(max_length=1000, blank=False)
-    task_details = models.CharField(max_length=1000, blank=False)
+    # task_details = models.CharField(max_length=1000, blank=False)
     category = models.ForeignKey(Category, related_name="tasks", on_delete=models.CASCADE)
 
     def __str__(self):
--- listBackend/task/serializers.py
+++ listBackend/task/serializers.py
@@ -3,6 +3,7 @@ from . import models
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
     class Meta:
         model=models.Task
         fields = ['task_id', 'task_title', 'task_details', 'category']
