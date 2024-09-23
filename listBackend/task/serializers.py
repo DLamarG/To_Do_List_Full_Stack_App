ff551821132f5b3be32e35443de053f04bacad51
DLamarG <demondg8@gmail.com>
2024-09-23 17:00:02 -0500

    added new field to task serializer

--- listBackend/task/serializers.py
+++ listBackend/task/serializers.py
@@ -5,4 +5,4 @@ from . import models
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model=models.Task
-        fields=['task_id'], ['task_title'], ['task_details']
+        fields = ['task_id', 'task_title', 'task_details', 'category']
