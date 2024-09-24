ef9503fb8a3dded7be3fec6b822a5efcd621965e
DLamarG <demondg8@gmail.com>
2024-09-23 20:21:34 -0500

    changed choices for categories

--- listBackend/category/models.py
+++ listBackend/category/models.py
@@ -4,13 +4,19 @@ from django.db import models
 
 
 TITLE_CHOICES = {
-    "Applications",
-    "Networking",
-    "Interviews",
-    "Coding",
-    "Projects",
-    "Certifications",
+    "A": "Applications",
+    "N": "Networking",
+    "I": "Interviews",
+    "C": "Coding",
+    "P": "Projects",
+    "D": "Development",
 }
+
+SHIRT_SIZES = {
+        "S": "Small",
+        "M": "Medium",
+        "L": "Large",
+    }
 
 
 class Category(models.Model):
